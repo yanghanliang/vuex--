2c2e80ad788af462e13979d14be3c8b9184312a2
Your Name <1218866243@qq.com>
2018-06-28 16:41:55 +0800

    映射的方式传值1和2

--- src/components/com-b.vue
+++ src/components/com-b.vue
@@ -3,16 +3,29 @@
         com-b
         <br />
         <!-- 计算属性的写法 -->
-        {{ test }}
+        <!-- {{ msg }} -->
+        {{ a }}
     </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   computed: {
-    test() {
-      return this.$store.state.msg
-    }
+    // test() {
+    //   return this.$store.state.msg
+    // }
+    // 映射的方式 1:
+    // this.msg --> this.$store.state.msg
+    // ...mapState([
+    //   'msg'
+    // ])
+    // 映射的方式 2:
+    // this.a --> this.$store.state.msg
+    ...mapState({
+      'a': 'msg'
+    })
   }
 }
 </script>
