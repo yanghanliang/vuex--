4d6276f16ed38e9671e041a69599bda114abc7e4
Your Name <1218866243@qq.com>
2018-06-28 17:24:20 +0800

    mutation 简化操作

--- src/components/com-c.vue
+++ src/components/com-c.vue
@@ -13,17 +13,31 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapMutations } from 'vuex'
 
 export default {
   methods: {
     hanleClick() {
       // 改变状态的值, 调用 mutation
-      this.$store.commit('setCount')
-    }
+      // this.$store.commit('setCount')
+      // this.setCount()
+      this.set()
+    },
+    // 映射 mutation
+    // this.setCount --> this.$store.commit('setCount')
+    // 方式 1
+    // ...mapMutations([
+    //   'setCount'
+    // ])
+    // 方式 2
+    // this.set --> this.$store.commit('setCount')
+    ...mapMutations({
+      'set': 'setCount'
+    })
   },
   // 展示数据
   computed: {
+    // 不能进行异步调用
     ...mapState([
       'count'
     ])
